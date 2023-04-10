9b9c072974be338795041a8f22bb052ef262dcb6
Luca Braun <l.braun@s-w-e.com>
2023-04-10 14:42:23 +0200

    [TASK] Added infos

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 import enablePersons from './modules/persons.js'
 
 const currentMonthNumber = parseInt(document.querySelector('#month').value)
-const table = ''
+const print = document.querySelector('#print')
 
+print.addEventListener('click', () => {
+  window.print()
+})
 enablePersons()
--- views/index.php
+++ views/index.php
@@ -60,6 +60,8 @@ if (isset($month)) {
     </form>
 </div>
 
+<button type="button" id="print">Drucken</button>
+
 <div class="py-4">
     <div class="mb-2 flex flex-row">
         <div>
@@ -81,7 +83,7 @@ if (isset($month)) {
         </form>
     </div>
 
-    <form method="post" action="/times" class="flex flex-col w-1/2">
+    <form method="post" action="/times" class="flex flex-col w-2/3">
         <?php
         $year = (int)date('Y');
 
@@ -91,11 +93,47 @@ if (isset($month)) {
                 $saveDate = date('Y-m-d', $time);
                 $date = datefmt_format($fmtDate, $time);
                 ?>
-                <div class="flex flex-row justify-between align-middle gap-2" data-row="<?php echo $d; ?>">
+                <div class="flex flex-row justify-between align-middle gap-2 my-2 pb-2 border-b border-b-slate-900" data-row="<?php echo $d; ?>">
                     <div>
                         <?php echo $date; ?>
                         <input name="date-<?php echo $d; ?>" type="hidden" value="<?php echo $saveDate; ?>">
                     </div>
+                    <div>
+                        <div>
+                            <label for="name-<?php echo $d; ?>">Name</label>
+                            <select name="name-<?php echo $d; ?>" id="name-<?php echo $d; ?>">
+                            </select>
+                        </div>
+                        <div>
+                            <label for="time-from-<?php echo $d; ?>">Von</label>
+                            <input type="time" name="time-from-<?php echo $d; ?>" id="time-from-<?php echo $d; ?>">
+                            <label for="time-to-<?php echo $d; ?>">Bis</label>
+                            <input type="time" name="time-to-<?php echo $d; ?>" id="time-to-<?php echo $d; ?>">
+                        </div>
+                    </div>
+                    <div data-result="<?php echo $d; ?>">
+                        0
+                    </div>
+
+
+                    <div>
+                        <div>
+                            <label for="name-<?php echo $d; ?>">Name</label>
+                            <select name="name-<?php echo $d; ?>" id="name-<?php echo $d; ?>">
+                            </select>
+                        </div>
+                        <div>
+                            <label for="time-from-<?php echo $d; ?>">Von</label>
+                            <input type="time" name="time-from-<?php echo $d; ?>" id="time-from-<?php echo $d; ?>">
+                            <label for="time-to-<?php echo $d; ?>">Bis</label>
+                            <input type="time" name="time-to-<?php echo $d; ?>" id="time-to-<?php echo $d; ?>">
+                        </div>
+                    </div>
+                    <div data-result="<?php echo $d; ?>">
+                        0
+                    </div>
+
+
                     <div>
                         <div>
                             <label for="name-<?php echo $d; ?>">Name</label>
